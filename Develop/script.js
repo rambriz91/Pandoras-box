6789b3ccf6223c57b0f8ba271ba79919247fe602
rambriz91 <rambriz91@gmail.com>
2023-01-14 04:45:53 -0800

    created generatePassword fxn in script.js.

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,14 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword() {
+  var passwordLength = prompt("Choose a length of at least 8 characters and no nore than 128 characters");
+  confirm("Include lowercase characters?");
+  confirm("Include uppercase characters?");
+  confirm("Include special characters?");
+  confirm("Include numbers?");
+  return "Password Generated"
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
