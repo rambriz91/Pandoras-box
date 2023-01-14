4afe86157f001d398ca03a5a001dfb6d15003cbe
rambriz91 <rambriz91@gmail.com>
2023-01-14 05:10:49 -0800

    Added arrays for lowercase, uppercase, special characters, and numbers.

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = lowerCase.toUpperCase();
+var numbers = ['1','2','3','4','5','6','7','8','9','0'];
+var specialChar = ['!','@','#','$','%','^','&','*','?'];
 
 // Write password to the #password input
 function generatePassword() {
-  var passwordLength = prompt("Choose a length of at least 8 characters and no nore than 128 characters");
+  var passwordLength = prompt("Choose a password length of at least 8 characters and no nore than 128 characters");
   confirm("Include lowercase characters?");
   confirm("Include uppercase characters?");
   confirm("Include special characters?");
